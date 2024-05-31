39c0259f4937e3f8dfac43f44d7be5c293e8bc22
arifuddinraihan <mdarifuddinraihan@gmail.com>
2024-05-31 13:04:27 +0600

    added dashboard page, layout, admin dashboard, user dashboard page

---
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const MainDashboardPage = () => {
+  return <div>Welcome to Dashboard Page.</div>;
+};
+
+export default MainDashboardPage;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+const GlobalNotFoundPage = () => {
+  return <div>Global Not Found Page, 404</div>;
+};
+
+export default GlobalNotFoundPage;
