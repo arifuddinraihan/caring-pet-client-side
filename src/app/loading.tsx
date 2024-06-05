069df5da82c07f7d3577f5661e54eab71f9a564b
arifuddinraihan <mdarifuddinraihan@gmail.com>
2024-06-06 01:10:57 +0600

    loading page fixed from admin route to global dashboard route

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,8 +3,13 @@ import { Spinner } from "@nextui-org/react";
 const LoadingPage = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="w-[90%] mx-auto">
-        <Spinner label="Loading" color="secondary" labelColor="secondary" size="lg" />
+      <div className="mx-auto text-center">
+        <Spinner
+          label="Loading"
+          color="secondary"
+          labelColor="secondary"
+          size="lg"
+        />
       </div>
     </div>
   );
